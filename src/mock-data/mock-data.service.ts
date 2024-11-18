d4cf188dd8ef3fdc723a8cf62b71bcd82378aa07
Rafael Luiz Siebeneichler <rafael.siebeneichler@caf.io>
2024-11-18 01:14:03 -0300

    fix: add logs in database ops

--- src/mock-data/mock-data.service.ts
+++ src/mock-data/mock-data.service.ts
@@ -14,6 +14,7 @@ export class MockDataService {
     try {
       return await this.mockDataModel.create(mockData);
     } catch (error) {
+      console.error('operation insert: ', error);
       if (error instanceof ValidationError) {
         throw new BadRequestException(error.errors.map(e => e.message).join(', '));
       }
@@ -22,11 +23,16 @@ export class MockDataService {
   }
 
   async findAll(offset: number, limit: number, order: 'asc' | 'desc' = 'desc'): Promise<MockData[]> {
-    return this.mockDataModel.findAll({
-      offset,
-      limit,
-      order: [['date', order]],
-    });
+    try {
+      return this.mockDataModel.findAll({
+        offset,
+        limit,
+        order: [['date', order]],
+      });
+    } catch (error) {
+      console.error('operation findall: ', error);
+      throw error;
+    }
   }
 
   async findOne(id: number): Promise<MockData> {
@@ -42,36 +48,40 @@ export class MockDataService {
   }
 
   async getSummary(account: number, period: 'month' | 'year'): Promise<any> {
-    const now = new Date();
-    let startDate: Date;
+    try {
+      const now = new Date();
+      let startDate: Date;
 
-    if (period === 'month') {
-      startDate = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
-    } else if (period === 'year') {
-      startDate = new Date(now.getFullYear() - 1, now.getMonth(), now.getDate());
-    } else {
-      throw new BadRequestException('Invalid period. Use "month" or "year".');
-    }
-    await this.mockDataModel.sequelize?.query('SELECT pg_sleep(5)');
+      if (period === 'month') {
+        startDate = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+      } else if (period === 'year') {
+        startDate = new Date(now.getFullYear() - 1, now.getMonth(), now.getDate());
+      } else {
+        throw new BadRequestException('Invalid period. Use "month" or "year".');
+      }
 
-    const results = await this.mockDataModel.findAll({
-      where: {
-        account,
-        date: {
-          [Op.gte]: startDate,
+      const results = await this.mockDataModel.findAll({
+        where: {
+          account,
+          date: {
+            [Op.gte]: startDate,
+          },
         },
-      },
-      attributes: [
-        'account',
-        [Sequelize.fn('SUM', Sequelize.literal(`CASE WHEN ind_dc = 'D' THEN value ELSE -value END`)), 'totalValue'],
-      ],
-      group: ['account'],
-    });
+        attributes: [
+          'account',
+          [Sequelize.fn('SUM', Sequelize.literal(`CASE WHEN ind_dc = 'D' THEN value ELSE -value END`)), 'totalValue'],
+        ],
+        group: ['account'],
+      });
 
-    if (results.length === 0) {
-      throw new NotFoundException(`No data found for account ${account} in the specified period.`);
+      if (results.length === 0) {
+        throw new NotFoundException(`No data found for account ${account} in the specified period.`);
+      }
+
+      return results[0];
+    } catch (error) {
+      console.error('operation summary: ', error);
+      throw error;
     }
-
-    return results[0];
   }
 }
